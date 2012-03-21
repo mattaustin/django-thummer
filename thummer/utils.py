3d63bf2dd9d519769a61406f7ec13a93e5c095c0
Matt Austin <mail@mattaustin.me.uk>
2012-03-21 20:40:42 +0800

    Added pre_delete signal for WebpageSnapshot, so that the image file (and thumbnails) are also deleted.

--- thummer/utils.py
+++ thummer/utils.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from thummer.models import WebpageSnapshot
 
 
 def get_thumbnail(url, geometry_string, **kwargs):
     """Finds or creates a valid ``WebpageSnapshot`` for the provided url,
     and returns its ``get_thumbnail`` method with the arguments provided."""
+    from thummer.models import WebpageSnapshot
     try:
         webpage_snapshot = WebpageSnapshot.objects.filter(
             url=url).valid().latest()
     except WebpageSnapshot.DoesNotExist:
         webpage_snapshot = WebpageSnapshot(url=url)
     return webpage_snapshot.get_thumbnail(geometry_string, **kwargs)
+
+
+def delete_image(sender, instance, **kwargs):
+    instance.image.delete(save=False)
 
