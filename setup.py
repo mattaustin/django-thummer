417c5c6b6e968ca2eb664e979e969bcf3078b729
Matt Austin <devops@mattaustin.com.au>
2018-02-07 19:19:57 +0800

    Switches to Apache license. Refactors setup.py in preparation for newer Django/Python compatibility and testing.

--- setup.py
+++ setup.py
@@ -1,29 +1,75 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
-from thummer.meta import VERSION
+from __future__ import absolute_import, unicode_literals
+
+import codecs
+from os import path
+
+from setuptools import find_packages, setup
+
+from thummer import __title__, __url__, __version__
 
 
-setup(name='django-thummer',
-    author='Matt Austin', author_email='mail@mattaustin.me.uk',
-    url='https://github.com/MattAustin/django-thummer',
+BASE_DIR = path.dirname(path.abspath(__file__))
+
+
+# Get the long description from the README file
+with codecs.open(path.join(BASE_DIR, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+
+    name=__title__,
+
+    version=__version__,
+
     description='A website screenshot and thumbnailing app for Django.',
-    long_description=open('README.rst').read(),
-    version=str(VERSION),
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=['django', 'python-dateutil>=2.0',
-        'pyvirtualdisplay>=0.1.0', 'selenium>=2.20.0',
-        'sorl-thumbnail>=11.12'],
-    license='BSD',
+    long_description=long_description,
+
+    url=__url__,
+
+    author='Matt Austin',
+    author_email='devops@mattaustin.com.au',
+
+    license='Apache',
+
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Framework :: Django',
+        'Topic :: Multimedia :: Graphics :: Capture',
     ],
-)
 
+    keywords='thummer django website snapshot screenshot thumbnail',
+
+    packages=find_packages(),
+
+    test_suite='thummer.tests',
+
+    install_requires=[
+        'django>=1.8,<1.9',
+        'python-dateutil>=2.6.1',
+        'selenium>=3.9.0',
+        'sorl-thumbnail>=12.4.1',
+    ],
+
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+
+    extras_require={
+        'tests': ['freezegun==0.3.9'],
+    },
+
+)
