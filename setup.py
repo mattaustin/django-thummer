e6edc30831aac3e2455c61cab0a98fcd3439924f
Matt Austin <devops@mattaustin.com.au>
2018-02-08 10:36:28 +0800

    Adds ``TestCommand`` to setup.py, which configures django with basic settings before running tests.

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import, unicode_literals
 import codecs
 from os import path
 
+from distutils.core import Command
 from setuptools import find_packages, setup
 
 from thummer import __title__, __url__, __version__
@@ -16,6 +17,35 @@ BASE_DIR = path.dirname(path.abspath(__file__))
 # Get the long description from the README file
 with codecs.open(path.join(BASE_DIR, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
+
+
+class TestCommand(Command):
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import django
+        from django.conf import settings
+        from django.core.management import call_command
+
+        settings.configure(
+            DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3'}},
+            INSTALLED_APPS=['thummer'],
+            MIDDLEWARE_CLASSES=[],
+            TEMPLATES=[{
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'APP_DIRS': True}],
+            MEDIA_ROOT='/tmp/django-thummer/media/',
+        )
+
+        django.setup()
+        call_command('test', 'thummer.tests', verbosity=3)
 
 
 setup(
@@ -57,13 +87,14 @@ setup(
 
     packages=find_packages(),
 
-    test_suite='thummer.tests',
+    cmdclass={'test': TestCommand},
 
     install_requires=[
-        'django>=1.8,<1.9',
-        'python-dateutil>=2.6.1',
-        'selenium>=3.9.0',
-        'sorl-thumbnail>=12.4.1',
+        'django>=1.8,!=1.9.*,!=1.10.*,<=2.1',
+        'pillow~=5.0',
+        'python-dateutil~=2.6',
+        'selenium~=3.9',
+        'sorl-thumbnail~=12.4',
     ],
 
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
