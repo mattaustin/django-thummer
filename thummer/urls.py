af96306344a8ab88e8709aeb9aa487033e53f629
Matt Austin <devops@mattaustin.com.au>
2018-02-08 18:41:30 +0800

    Refactors urls, make compatible with newer django versions.

--- thummer/urls.py
+++ thummer/urls.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls.defaults import include, patterns, url
-
 from . import views
 
 
-urls = patterns(
+try:
+    from django.urls import path_re
+except ImportError:
+    from django.conf.urls import url as path_re
 
-    'thummer.views',
 
-    url(r'^(?P<width>\d+)/(?P<height>\d+)/(?P<crop>0|1)/(?P<scheme>https?)://?(?P<hierarchical_part>.*)$',  # noqa: E501
-        views.ThumbnailView.as_view(), name='thumbnail'),
+app_label = 'thummer'
 
-)
 
-urlpatterns = patterns('', url(r'^', include(urls, namespace='thummer')))
+urlpatterns = [
+
+    path_re(r'^(?P<width>\d+)/(?P<height>\d+)/(?P<crop>0|1)/(?P<scheme>https?)://?(?P<hierarchical_part>.*)$',  # noqa: E501
+            views.ThumbnailView.as_view(), name='thumbnail'),
+
+]
